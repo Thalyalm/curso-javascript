11d7198ce740e92bfde6fc00832284020e9dfd79
Thalyson Almeida <th.diogo04@gmail.com>
2023-02-07 17:21:57 -0300

    ex008

---
+++ modulos/exercicios/ex008/EX008.js
@@ -0,0 +1,10 @@
+//console.log('O console funcionou corretamente!')
+
+var velocidade = 70
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`)
+
+if (velocidade > 60) { //condicação simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre com cinto de segurança!')
