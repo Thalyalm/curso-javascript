5e9a8941fcd70fdf378525e80e24e4cfa5c51fbd
Thalyson Almeida <th.diogo04@gmail.com>
2023-02-08 18:00:45 -0300

    script

--- modulos/exercicios/ex015/script.js
+++ modulos/exercicios/ex015/script.js
@@ -1,1 +1,15 @@
-alert('Olá!')
+function carregar() {
+    var horario = window.document.getElementById('hora')
+    var msg = window.document.getElementById('msg')
+    var data = new Date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+    msg.innerHTML = `${hora}:${minuto}`
+    if (hora < 12) {
+        ('Bom dia!')
+    } else if (hora < 18) {
+        ('Boa tarde!')
+    } else {
+        ('Boa noite!')
+    }
+}
