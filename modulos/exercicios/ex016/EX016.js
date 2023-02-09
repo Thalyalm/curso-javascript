38249929dbce984b2c4e1832cfeec1b04100c157
Thalyson Almeida <th.diogo04@gmail.com>
2023-02-09 09:21:43 -0300

    ex016

---
+++ modulos/exercicios/ex016/EX016.js
@@ -0,0 +1,23 @@
+var contador = 1
+
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 3)
+
+
+/*
+var contador = 1
+
+while (contador <= 3) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+*/
+
+
+/*
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+*/
