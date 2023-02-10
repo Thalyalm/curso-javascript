a38cbffad8ca3d9278f4a50c3c1d87f5f71acab5
Thalyson Almeida <th.diogo04@gmail.com>
2023-02-10 11:44:51 -0300

    ex022

---
+++ modulos/exercicios/ex022/EX022.js
@@ -0,0 +1,9 @@
+let valores = [0, 1, 2, 3]
+
+let posicao = valores.indexOf(5)
+
+if (posicao == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor 3 está na posição ${posicao}`)
+}
