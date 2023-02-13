16d8a518ab5c36065fc5a83dbd05658a4bf1d244
Thalyson Almeida <th.diogo04@gmail.com>
2023-02-13 11:39:39 -0300

    ex022

---
+++ exercicios/ex023/EX022.js
@@ -0,0 +1,12 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+console.log(resultado = parimpar(55))
+
+//resultado = parimpar(5)
+//console.log(resultado)
